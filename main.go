c34d2a99f81b8ca1db1132f94ff0a4c9bfc6f7bd
nabeel ahmed <nab2320@gmail.com>
2024-11-24 11:04:52 -0500

    d

--- main.go
+++ main.go
@@ -1266,6 +1266,7 @@ func GetCCVIDetails(db *sql.DB) {
 		ccvi_category := cciv_data_list[i].CCVI_category
 		rank_socioeconomic_status := cciv_data_list[i].rank_socioeconomic_status
 		rank_household_composition := cciv_data_list[i].rank_household_composition
+		fmt.Println(rank_socioeconomic_status)
 		rank_adults_no_pcp := cciv_data_list[i].rank_adults_no_pcp
 		rank_cumulative_mobility_ratio := cciv_data_list[i].rank_cumulative_mobility_ratio
 		rank_frontline_essential_workers := cciv_data_list[i].rank_frontline_essential_workers
