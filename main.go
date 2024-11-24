05c6a5b4cbd8d9df1096084b171954c2ecf45b2f
nabeel ahmed <nab2320@gmail.com>
2024-11-24 17:13:52 -0500

    up

--- main.go
+++ main.go
@@ -859,7 +859,7 @@ func GetBuildingPermits(db *sql.DB) {
 
 	// While doing unit-testing keep the limit value to 500
 	// later you could change it to 1000, 2000, 10,000, etc.
-	var url = "https://data.cityofchicago.org/resource/ydr8-5enu.json?$limit=500"
+	var url = "https://data.cityofchicago.org/resource/ydr8-5enu.json?$limit=2000"
 
 	tr := &http.Transport{
 		MaxIdleConns:       10,
